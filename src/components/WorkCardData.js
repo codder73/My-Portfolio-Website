83fc62f22510d5a670252a690a608f8524db7585
Coder-73 <117440836+Coder-73@users.noreply.github.com>
2022-11-04 14:55:16 +0530

    Changed the links.
    Helped myself...

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -14,15 +14,15 @@ const ProjectCardData = [
         imgsrc: pro2,
         title: "React Js First Project",
         text: "On our web, 97% percent of websites are made using JavaScript. And Reactjs is a great front-end module of JavaScript that is used to make single-page websites. It gives excellent performance and great experience to the users and to developers also. So then I learned Reactjs and made a Todo's App, which takes todo items from the user and saves them, when the user does that he can remove and add everything easily. It is a straightforward app. And now, I don't want to write more about Reactjs and Javascript in this area, because there are a lot of things to write about Reactjs and Javascript. But I want to Recommend you to learn Reactjs and Javascript. Because these will gain a lot of attention in the future. To know more about Reactjs, please visit",
-        view: "https://github.com/codder73/Youtube-Video-Downloader",
-        source: "https://github.com/codder73/Youtube-Video-Downloader"
+        view: "https://github.com/codder73/Todo-s-List",
+        source: "https://github.com/codder73/Todo-s-List"
     },
     {
         imgsrc: pro3,
         title: "My 3Dimensional Class Room in my computer",
         text: "In my school, A child gets admission in the 4th class and gets passed out of the school in the 12th class. When they pass out Every Student then gets an indirect connection to everything present in school like with the classmates, the school building, hostel, etc. And after passing out of school, everyone is not able to come to school and view it. So 3D, VR, etc. are the kind of technologies, that can help them to view these things online at the next level. So then we will get a lot of benefits alike- A person will come to school and he will have to come in the day time, he will have to quit work for one day. And major problem our environment is damaged a lot day by day, from transport pollution, etc. And we are also having a scarcity of fuel on our earth. We can make a 3d model of anything. And the talk of humans. This will be available soon in VR, those will be called metahumans. My essay is getting long, so I would like to say that 'We Must Change Ourself With Time...'",  
-        view: "https://github.com/codder73/Youtube-Video-Downloader",
-        source: "https://github.com/codder73/Youtube-Video-Downloader"
+        view: "https://github.com/codder73/My-3d-Blender-Projects",
+        source: "https://github.com/codder73/My-3d-Blender-Projects"
     }
 ]
 
